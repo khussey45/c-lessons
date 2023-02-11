916ba84df3dbbb0644aeaaee0c0c4dd1bfb73523
khussey45 <khussey45@gmail.com>
2023-02-10 20:14:54 -0500

    3rd

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,26 +6,57 @@
 
 
 
-   // FUNCTIONS
 
-void birthday(char name[], int age) {
- 
-    
-    
-  
-}
 
 
 int main(){
 
-    
+  
+    // for loop  = repeats a section of code a limited amount of times
     
 
 
+
+   
+    
     return 0;
 
+} 
 
 
+
+
+
+
+ // functions protype
+
+    // what is it? 
+    // function declaration, w/o a body before the main()
+    // Ensure that calls to a function are made with the correct arguments
+
+    // IMPORTANT NOTES
+    // Many C compilers do not check for parameters matching 
+    // Missing arguments will result in unexpected behaviour
+    // A function prototype causes the compiler to flag an error if arguments are missing
+
+    // ADVANTAGES
+    // 1. Easier to naviagate a program w/ main() at the top 
+    // 2. Helps with debugging
+    // 3. Commonly used in header files
+
+
+      // ternary operator = shortcut to if/else when assigning/returning a value
+    // (condition) ? value if true : value if false
+
+    
+// int findMax(int x, int y) {
+    
+//     return (x > y) ? x : y;
+// }
+
+//     int max =  findMax(1, 4);
+
+//     printf("%d", max);
 
 
     // format specifiers % = defines and formats a type of data to be displayed
@@ -278,4 +309,3 @@ int main(){
     
 // test
  
-}
