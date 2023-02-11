0ff420a1a34831aee896e27d83266ca3a4b5dec5
khussey45 <khussey45@gmail.com>
2023-02-11 16:13:36 -0500

    j

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,17 +3,27 @@
 #include <math.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <time.h>
 
 
 
 
 int main() {
 
-    
+    // pseudo random numbers = A set of values or elements that are statistically random (Dont use these for any sort of cryptographic security)
 
+    srand(time(0));
+
+    int number1 = (rand() % 6) + 1;
+
+    printf("%d", number1);
+
+    
 
     return 0;
 }
+
+
 
 // enum = a user defined type of named integer identifiers
     // helps to make a program more readable
